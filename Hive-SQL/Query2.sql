3c18c6b5909641cdf8f4b7478f420322a62089e2
Elisey <elisey999@gmail.com>
2022-10-26 15:39:53 +0300

    fix

--- Hive-SQL/Query2.sql
+++ Hive-SQL/Query2.sql
@@ -1,16 +1,17 @@
 --Самый популярный тэг на ластфм
 WITH S AS (
-    SELECT artist_lastfm, SPLIT(tags_lastfm, ';') as tags
+    SELECT artist_lastfm, SPLIT(tags_lastfm, ';') as tags, listeners_lastfm
     FROM artists
 ),  
 T AS (
-    SELECT S.artist_lastfm, T.tag
+    SELECT S.artist_lastfm, tag, S.listeners_lastfm
     FROM S
     LATERAL VIEW explode(tags) t AS tag
 )
-SELECT T.tag, count(T.tag) as times_occured 
-FROM T 
+SELECT T.tag, SUM(T.listeners_lastfm) as popularity 
+FROM T
 WHERE T.tag <> '' and T.tag IS NOT NULL
-GROUP BY T.tag 
-ORDER BY times_occured DESC 
+GROUP BY T.tag
+ORDER BY popularity DESC
 LIMIT 1;
+
--- Hive-SQL/Query3.sql
+++ Hive-SQL/Query3.sql
@@ -1,23 +1,23 @@
 --Самые популярные исполнители 10 самых популярных тегов ластфм
 WITH S AS (
-    SELECT artist_lastfm, SPLIT(tags_lastfm, ';') AS tags, listeners_lastfm
+    SELECT artist_lastfm, SPLIT(tags_lastfm, ';') as tags, listeners_lastfm
     FROM artists
 ),  
 T AS (
-    SELECT S.artist_lastfm, T.tag AS tag, S.listeners_lastfm
+    SELECT S.artist_lastfm, tag, S.listeners_lastfm
     FROM S
     LATERAL VIEW explode(tags) t AS tag
 ),
 R AS (
-    SELECT T.tag AS tag, count(T.tag) AS times_occured
-    FROM T 
+    SELECT T.tag AS Tag, SUM(T.listeners_lastfm) as popularity 
+    FROM T
     WHERE T.tag <> '' and T.tag IS NOT NULL
     GROUP BY T.tag
-    ORDER BY times_occured DESC 
+    ORDER BY popularity DESC
     LIMIT 10
 ),
 M AS (
-    SELECT T.artist_lastfm, T.tag, T.listeners_lastfm, R.times_occured 
+    SELECT T.artist_lastfm, T.tag, T.listeners_lastfm, R.popularity
     FROM R 
     JOIN T 
     ON T.tag = R.tag
@@ -27,8 +27,9 @@ L AS (
     FROM M
     GROUP BY M.tag
 )
-SELECT M.tag AS tag, M.artist_lastfm AS artist, M.times_occured AS tag_count
+SELECT M.tag AS tag, M.artist_lastfm AS artist, M.popularity AS tag_listeners
 FROM M
-JOIN L ON L.tag = M.tag and L.max_listeners = M.listeners_lastfm
-ORDER BY tag_count
+JOIN L 
+ON L.tag = M.tag and L.max_listeners = M.listeners_lastfm
+ORDER BY tag_listeners
 DESC;
